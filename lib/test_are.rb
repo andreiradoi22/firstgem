943085343085d712918bf2ac15ec9ad30018c86b
andreiradoi22 <andrei@mytime.com>
2022-09-07 13:52:15 +0300

    my first commit

--- lib/test_are.rb
+++ lib/test_are.rb
@@ -2,7 +2,22 @@
 
 require_relative "test_are/version"
 
+# com
 module TestAre
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.convert(amount, from:, to:)
+    case from
+    when :mi
+      case to
+      when :km
+        amount * 1.609344
+      end
+    when :km
+      case to
+      when :mi
+        amount / 1.609344
+      end
+    end
+  end
 end
--- spec/test_are_spec.rb
+++ spec/test_are_spec.rb
@@ -5,7 +5,13 @@ RSpec.describe TestAre do
     expect(TestAre::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe ".convert" do
+    it "conv to kilometers" do
+      expect(TestAre.convert(3.1, from: :mi, to: :km).round(1)).to eql(5.0)
+    end
+
+    it "conv kilometers to miles" do
+      expect(TestAre.convert(5, from: :km, to: :mi).round(1)).to eql(3.1)
+    end
   end
 end
