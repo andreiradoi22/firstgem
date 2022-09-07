6f224e84765b447460f82eb2d85465c1ad84babe
Andrei <andrei@mytime.com>
2022-09-07 13:31:51 +0300

    First gem (convert function)

---
+++ spec/test_are_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe TestAre do
+  it "has a version number" do
+    expect(TestAre::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
